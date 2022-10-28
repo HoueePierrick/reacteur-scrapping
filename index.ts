46516d5ac167c1b1998c18d74ed0ac90263ccfe6
PierrickHOUEE <houee.pierrick@gmail.com>
2022-10-28 17:59:54 +0200

    solved

--- index.ts
+++ index.ts
@@ -3,11 +3,17 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://developers.google.com/web/");
+  await page.goto("https://www.escapegame.fr/paris/");
 
   // Get the "viewport" of the page, as reported by the page
   const games = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("#jsRooms article"));
+    return Array.from(document.querySelectorAll("#jsRooms article")).map(
+      (el) => {
+        return {
+          name: el.querySelector(".card-title a")?.textContent,
+        };
+      }
+    );
   });
   console.log(games);
   await browser.close();
