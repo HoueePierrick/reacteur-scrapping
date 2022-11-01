4c5b69a4bcb330c9a4d904327b50ef5f6e08690e
Pierrick Houée <houee.pierrick@gmail.com>
2022-11-01 19:08:45 +0100

    pending

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
+const getGames = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.escapegame.fr/paris/");
+  await page.goto("https://www.escapegame.fr/paris/", {
+    waitUntil: "networkidle2",
+  });
 
   // Get the "viewport" of the page, as reported by the page
   const games = await page.evaluate(() => {
@@ -52,11 +54,62 @@ const puppeteer = require("puppeteer");
           "user-rating":
             el.querySelector(".user-rating") &&
             // Travailler sur ça
-            el.querySelector(".user-rating")?.textContent?.trim().split(" "),
+            Number(
+              el
+                .querySelector(".user-rating")
+                ?.textContent?.trim()
+                .split(/\s/)[0]
+                .slice(0, -1)
+            ),
+          thumbnail: el
+            .querySelector(".card-room-hero-image > img")
+            ?.getAttribute("src")
+            ?.split("?")[0],
+          domain:
+            el.querySelector(".card-link")?.getAttribute("href") &&
+            `https://www.escapegame.fr/paris${el
+              .querySelector(".card-link")
+              ?.getAttribute("href")}`.slice(0, 5) === `https`
+              ? `https://www.escapegame.fr/paris${el
+                  .querySelector(".card-link")
+                  ?.getAttribute("href")
+                  ?.substring(6)}`
+              : `https://www.escapegame.fr/paris${el
+                  .querySelector(".card-link")
+                  ?.getAttribute("href")}`,
         };
       }
     );
   });
-  console.log(games);
   await browser.close();
-})();
+  return games;
+};
+
+// getGames();
+
+const getMoreAboutGames = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.escapegame.fr/paris/quest-factory/cannibal-island/",
+    { waitUntil: "networkidle2" }
+  );
+
+  const myFunction = (node: HTMLElement | null) => {
+    if (node) {
+      return node.innerText; // Property 'innerText' does not exist on 'T'
+    }
+  };
+
+  const summary = await page.evaluate(() => {
+    // return myFunction(document.querySelector(".content"));
+    const node = document.querySelector(".content") as HTMLElement;
+    return node.innerText;
+  });
+
+  console.log({ summary });
+
+  await browser.close();
+};
+
+getMoreAboutGames();
